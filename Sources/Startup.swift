34866626a09a3c3efffdc5531e466bf1ce1670ad
Pedro Piñera Buendía <pepibumur@gmail.com>
2017-04-29 19:19:06 +0200

    Update Startup to add library top level functions

--- Sources/Startup.swift
+++ Sources/Startup.swift
@@ -1,9 +1,22 @@
-//
-//  Startup.swift
-//  Startup
-//
-//  Created by Pedro Piñera Buendía on {TODAY}.
-//  Copyright © 2017 Startup. All rights reserved.
-//
-
 import Foundation
+
+
+/// Executes the operation.
+///
+/// - Parameters:
+///   - operation: operation to be executed.
+///   - completion: closure that will be called once the execution completes.
+public func startup(operation: Operation, completion: @escaping (Error?) -> ()) {
+    let queue = DispatchQueue.global(qos: .background)
+    operation.execute(in: queue, completion: completion)
+}
+
+/// Executes the the operation operations in serie
+///
+/// - Parameters:
+///   - operations: operations to be executed sequencially.
+///   - completion: closure that will be called once all the operations complete.
+public func startup(operations: [Operation], completion: @escaping (Error?) -> ()) {
+    let serialOperation = SerialOperation(operations: operations)
+    startup(operation: serialOperation, completion: completion)
+}
