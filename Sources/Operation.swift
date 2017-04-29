763859f699944d0a107a17cc4c078237b19556dd
Pedro Piñera Buendía <pepibumur@gmail.com>
2017-04-29 19:18:29 +0200

    Define Operation protocol

---
+++ Sources/Operation.swift
@@ -0,0 +1,73 @@
+import Foundation
+
+/// Protocol that defines an operation to be executed.
+public protocol Operation: CustomStringConvertible {
+    
+    /// Name of the operation that will be executed.
+    /// Name will be useful when debugging the execution of the operations.
+    var description: String { get }
+    
+    /// Executes all the operations serially.
+    ///
+    /// - Throws: an error if any of the operations fails.
+    func execute() throws
+    
+}
+
+// MARK: - Operation Extension (Queueing)
+
+internal extension Operation {
+    
+    /// Executes the operation asynchronously in the given queue
+    ///
+    /// - Parameters:
+    ///   - queue: queue where the operation will be executed.
+    ///   - completion: closure that will be called once the operation completes.
+    func execute(in queue: DispatchQueue, completion: @escaping (Error?) -> ()) {
+        queue.async {
+            do {
+                try self.execute()
+                completion(nil)
+            } catch {
+                completion(error)
+            }
+        }
+    }
+    
+}
+
+// MARK: - Operation Extension (Error Handling)
+
+public extension Operation {
+    
+    /// It returns another operation that retries if the wrapped operation fails.
+    ///
+    /// - Parameter retry: number of times it should be retried.
+    /// - Returns: operation that retries the execution in case it fails.
+    func `catch`(retry: UInt) -> Operation {
+        return AnyOperation(description: self.description, closure: {
+            for i in 0..<retry {
+                do {
+                    try self.execute()
+                } catch {
+                    if i == retry - 1 {
+                        throw error
+                    }
+                }
+            }
+        })
+    }
+    
+    /// Returns an operation that silences the error.
+    ///
+    /// - Returns: operation that silences the error if the wrapped operation throws any.
+    func silent() -> Operation {
+        return AnyOperation(description: self.description, closure: {
+            do {
+                try self.execute()
+            } catch {}
+        })
+    }
+    
+    
+}
