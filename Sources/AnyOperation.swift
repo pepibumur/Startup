ec50938b19db85505c31b40a9839cd44b6fa6a2d
Pedro Piñera Buendía <pepibumur@gmail.com>
2017-04-29 19:18:37 +0200

    Define AnyOperation

---
+++ Sources/AnyOperation.swift
@@ -0,0 +1,29 @@
+import Foundation
+
+/// Operation that is initialized with the action that has to be executd.
+struct AnyOperation: Operation {
+    
+    /// Operation description
+    var description: String
+    
+    /// Action to be executed.
+    private let closure: () throws -> ()
+    
+    /// Initializes the operation with the name and the action to be executed.
+    ///
+    /// - Parameters:
+    ///   - description: operation description.
+    ///   - closure: action to be executed.
+    init(description: String, closure: @escaping () throws -> ()) {
+        self.description = "Operation: \(description)"
+        self.closure = closure
+    }
+    
+    /// Executes the operation that has been passed as a closure.
+    ///
+    /// - Throws: throws an error if the operation fails.
+    func execute() throws {
+        try self.closure()
+    }
+    
+}
